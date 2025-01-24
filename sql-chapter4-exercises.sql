407e5c01813b44d994534e4e87aef5d2c7fa78ab
adrian-johnson <4corderoj@gmail.com>
2025-01-24 15:52:28 -0500

    up to exercise 4

---
+++ sql-chapter4-exercises.sql
@@ -0,0 +1,49 @@
+--Chapter 4 Exercises -p. 132, 133
+USE AP;
+
+--Exercise 1
+-- Write a SELECT statment that return all columns from:
+--Vendors tables inner-joined with the invoices table
+SELECT *
+	FROM Invoices I
+	JOIN Vendors V ON V.VendorID = I.VendorID 
+	ORDER BY V.VendorID;
+--Exercise 2
+-- Write a SELECT statement that returns four columns:
+-- 1) VendorName > From V table
+-- 2) InvoiceNumber > From I table
+-- 3) InvoiceDate > From I table
+-- 4) Balance > InvoiceTotal - (PaymentTotal + CreditTotal)
+SELECT *
+	FROM Invoices;
+SELECT *
+	FROM Vendors;
+
+SELECT VendorName, InvoiceNumber, InvoiceDate, InvoiceTotal - (PaymentTotal + CreditTotal) AS Balance
+	FROM Invoices I
+	JOIN Vendors V ON V.VendorID = I.VendorID
+	WHERE InvoiceTotal - (PaymentTotal + CreditTotal) <> 0
+	ORDER BY V.VendorName;
+-- I feel like I'd want the Vendor ID displayed as well for easy reference in other tables when used as a foriegn key but I had difficulties doing both.
+-- OK I figured it out by adding V.VendorID on the SELECT line
+
+--Exercise 3
+-- Write a SELECT statement that return three columns:
+-- VendorName > V table
+--DefaultAccountNo > V table
+-- AccountDescription > GLAccounts (GLA) table
+SELECT VendorName, DefaultAccountNo
+	FROM Vendors;
+
+SELECT VendorName, DefaultAccountNo, AccountDescription
+	FROM GLAccounts GLA
+	JOIN Vendors V ON V.DefaultAccountNo = GLA.AccountNo;
+--Sniff out what they have in common
+
+--Exercise 4
+--Generate the same result set described in ex. 2, but use:
+--the implicit join syntax -p. 115
+
+
+
+	
