c6b0c5353bfaf5b3880d29ff5465ee9c78f33aab
adrian-johnson <4corderoj@gmail.com>
2025-03-11 16:31:17 -0400

    sql changes

---
+++ prsquest-my-sql/prsquest-create-mysql.sql
@@ -0,0 +1,81 @@
+-- create and select the database
+DROP DATABASE IF EXISTS prsquest;
+CREATE DATABASE prsquest;
+USE prsquest;
+
+-- create User table
+-- DROP TABLE IF EXISTS User;
+Create table User (
+ID				INT				primary key auto_increment,
+Username		VARCHAR(20)		NOT NULL,
+Password		VARCHAR(10)		NOT NULL,
+FirstName		VARCHAR(20)		NOT NULL,
+LastName		VARCHAR(20)		NOT NULL,
+PhoneNumber		VARCHAR(12)		NOT NULL,
+Email			VARCHAR(75)		NOT NULL,
+CharacterName	VARCHAR(20)		NOT NULL,
+Class			VARCHAR(25)		NOT NULL,
+Title			VARCHAR(50)		NULL,
+Reviewer		BIT				NOT NULL,
+Admin			BIT				NOT NULL,
+CONSTRAINT uname UNIQUE (Username),
+CONSTRAINT charname UNIQUE (CharacterName)
+);
+
+-- create Vendor table
+-- DROP TABLE IF EXISTS Vendor;
+Create table Vendor(
+ID				INT					primary key auto_increment,
+Code			VARCHAR(10)			NOT NULL,
+Name			VARCHAR(255)		NOT NULL,
+Address			VARCHAR(255)		NOT NULL,
+City			VARCHAR(255)		NOT NULL,
+State			VARCHAR(2)			NOT NULL,
+Zip				VARCHAR(5)			NOT NULL,
+PhoneNumber		VARCHAR(12)			NOT NULL,
+Email			VARCHAR(100)		NOT NULL,
+CONSTRAINT vcode UNIQUE (Code)
+);
+
+-- create Product table
+-- DROP TABLE IF EXISTS Product;
+Create table Product(
+ID				INT				primary key auto_increment,
+VendorID		INT				NOT NULL,
+PartNumber		VARCHAR(50)		NOT NULL,
+Name			VARCHAR(150)	NOT NULL,
+Price			DECIMAL(10,2)	NOT NULL,
+Unit			VARCHAR(255)	NULL,
+PhotoPath		VARCHAR(255)	NULL,
+CONSTRAINT vendorpart UNIQUE (VendorID, PartNumber),
+CONSTRAINT FK_ProductVendor FOREIGN KEY (VendorID) REFERENCES Vendor(ID)
+);
+
+-- create Vendor table
+-- DROP TABLE IF EXISTS Vendor;
+Create table Request(
+ID					INT				primary key auto_increment,
+UserID				INT				NOT NULL,
+RequestNumber		VARCHAR(20)		NOT NULL,
+Description			VARCHAR(100)	NOT NULL,
+Justification		VARCHAR(255)	NOT NULL,
+DateNeeded			DATE			NOT NULL,			
+DeliveryMode		VARCHAR(25)		NOT NULL,
+Status				VARCHAR(20)		DEFAULT 'NEW',	
+Total				DECIMAL(10,2)	DEFAULT 0.0,
+SubmittedDate		DATETIME		NOT NULL,
+ReasonForRejection	VARCHAR(100)	NULL,
+CONSTRAINT FK_RequestUser FOREIGN KEY (UserID) REFERENCES User(ID)
+);
+
+-- create Vendor table
+-- DROP TABLE IF EXISTS Vendor;
+Create table LineItem(
+ID				INT		primary key auto_increment,	
+RequestID		INT		NOT NULL,
+ProductID		INT		NOT NULL,
+Quantity		INT		NOT NULL,
+CONSTRAINT reqpdt UNIQUE (RequestID, ProductID),
+CONSTRAINT FK_LineReq FOREIGN KEY (RequestID) REFERENCES Request(ID),
+CONSTRAINT FK_LinePro FOREIGN KEY (ProductID) REFERENCES Product(ID)
+);
--- prsquest/prsquest-insert-query-sql.sql
+++ prsquest/prsquest-insert-query-sql.sql
@@ -1,25 +1,25 @@
---prsquest insert query
+-- prsquest insert query
 
 USE prsquest;
 
---insert 2 users
+-- insert 2 users
 
 SELECT *
-	FROM [User]
+	FROM User;
 
-INSERT INTO [User] (Username, Password, FirstName, LastName, PhoneNumber, Email, CharacterName, Class, Title, Reviewer, Admin)
+INSERT INTO User (Username, Password, FirstName, LastName, PhoneNumber, Email, CharacterName, Class, Title, Reviewer, Admin)
 VALUES
 	('Adrian', 'password', 'Adrian', 'Johnson', '318-900-1988', 'Adriatticus@prsguild.com', 'Adriatticus', 'Paladin', 'Guild Master', 0, 1),
 	('Travis', 'changeme', 'Travis', 'Boettjer', '909-900-1988', 'Travatticus@prsguild.com', 'Travatticus', 'Mageblade', 'The Patient', 1, 0);
 
---Unused User
+-- Unused User
 
 /*INSERT INTO [User] (Username, Password, FirstName, LastName, PhoneNumber, Email, CharacterName, Class, Title, Reviewer, Admin)
 VALUES
 	('Matt', 'password', 'Matt', 'Houp', '311-900-1988', 'Matatticus@prsguild.com', 'Matatticus', 'Death Knight', 'Blade of Ruin', 1, 0); */
 
 SELECT *
-	FROM [User];
+	FROM User;
 SELECT *
 	FROM Vendor;
 
@@ -32,13 +32,13 @@ VALUES
 	('OF', 'Simon the Outfitter', '12 Wildlife Corridor', 'Hunters Haven', 'KW',/*Kingswood*/'51141','415-585-5114','Simon@OFGuild.com');
 
 
---I need 10 products -- Game Jerky, Flint and Tinder, Leather Flask, Health Potion, Enchanted Gnarled Staff, Scroll of Water Breathing, Bronze Sword, Hero's Shield, Adventurer's Breastplate, Four Leather Belt
+-- I need 10 products -- Game Jerky, Flint and Tinder, Leather Flask, Health Potion, Enchanted Gnarled Staff, Scroll of Water Breathing, Bronze Sword, Hero's Shield, Adventurer's Breastplate, Four Leather Belt
 
 SELECT *
-	FROM [Product]
+	FROM Product
  ORDER BY Price;
 
-INSERT INTO [Product] (VendorID, Partnumber, Name, Price, Unit)
+INSERT INTO Product (VendorID, Partnumber, Name, Price, Unit)
 VALUES
 	(3, '1', 'Game Jerky', 0.55, '1'),
 	(3, '2', 'Leather Flask', 2.50, '1'),
@@ -64,10 +64,10 @@ INSERT INTO LineItem (RequestID, ProductID, Quantity)
 VALUES
 	(3, 7, 1);
 
---Adriatticus Wants to buy a Scroll of Water Breathing for 15g
+-- Adriatticus Wants to buy a Scroll of Water Breathing for 15g
 
 SELECT CharacterName, P.Name, R.Total
 	FROM Request R
 	JOIN LineItem LI ON R.ID = LI.RequestID
 	JOIN Product P ON P.ID = LI.ProductID
-	JOIN [User] U ON U.ID = R.UserID;
+	JOIN User U ON U.ID = R.UserID;
